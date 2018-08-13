7eba6e2d7319a41d9e04d3b84ad7bef344d74133
yungshun317 <seraph317@gmail.com>
2018-08-13 14:17:30 +0800

    Use repartitionAndSortWithinPartitions with the custom CompositeKeyPartitioner partitioner to repartition and sort the data by the key

--- src/main/scala/yungshun/chang/movingaverage/MovingAverage.scala
+++ src/main/scala/yungshun/chang/movingaverage/MovingAverage.scala
@@ -28,6 +28,8 @@ object MovingAverage {
       val timestamp = dateFormat.parse(tokens(1)).getTime
       (CompositeKey(tokens(0), timestamp), TimeSeriesData(timestamp, tokens(2).toDouble))
     })
+
+    val sortedData = valueTokey.repartitionAndSortWithinPartitions(new CompositeKeyPartitioner(numPartitions))
   }
 }
 
@@ -38,4 +40,25 @@ object CompositeKey {
   implicit def ordering[A <: CompositeKey]: Ordering[A] = {
     Ordering.by(fk => (fk.stockSymbol, fk.timeStamp))
   }
+}
+
+import org.apache.spark.Partitioner
+
+class CompositeKeyPartitioner(partitions: Int) extends Partitioner {
+  require(partitions >= 0, s"Number of partitions ($partitions) cannot be negative.")
+
+  def numPartitions: Int = partitions
+
+  def getPartition(key: Any): Int = key match {
+    case k: CompositeKey => math.abs(k.stockSymbol.hashCode % numPartitions)
+    case null            => 0
+    case _               => math.abs(key.hashCode % numPartitions)
+  }
+
+  override def equals(other: Any): Boolean = other match {
+    case h: CompositeKeyPartitioner => h.numPartitions == numPartitions
+    case _                          => false
+  }
+
+  override def hashCode: Int = numPartitions
 }
